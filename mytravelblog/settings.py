96227b4bf8201644cb4c368fd5839d25c4c01812
yetoshimo <yetmenyilmaz@gmail.com>
2022-04-09 19:35:30 +0300

    corrected media root in settings.py

--- mytravelblog/settings.py
+++ mytravelblog/settings.py
@@ -138,7 +138,7 @@ else:
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'mediafiles/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
