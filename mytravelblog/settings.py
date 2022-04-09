5feefab42f2bd1b298fe275a6cf08acf9f84aad0
yetoshimo <yetmenyilmaz@gmail.com>
2022-04-09 17:32:03 +0300

    corrected static settings in settings.py

--- mytravelblog/settings.py
+++ mytravelblog/settings.py
@@ -128,9 +128,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 if not DEBUG:
-    STATIC_ROOT = (
-        os.path.join(BASE_DIR, 'staticfiles')
-    )
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 else:
     STATICFILES_DIRS = (
         BASE_DIR / 'staticfiles',
